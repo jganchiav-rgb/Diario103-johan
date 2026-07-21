9f069fee0011bb139f557d6ad893fb2772ec18a1
jganchiav-rgb <jganchiav@ctpplatanares.ed.cr>
2026-07-21 07:17:50 -0300

    listas de emprendimientos
    ejercicios de listas de emprendimiento

--- RA5/RA5_analisis_emprendimientos.py
+++ RA5/RA5_analisis_emprendimientos.py
@@ -4,5 +4,10 @@ Complete los espacios marcados con TODO. El objetivo es generar un reporte por
 sede usando listas, diccionarios, funciones, ciclos y condicionales.
 """
 
-from sedes import sedes
+from sedes import sedes  
+
+print("la variable sedes es tipo:", type(sedes)._name_)
+primer_emprendimiento = sedes[0]
+print("primer emprendimiento:", primer_emprendimiento)
+print("tipo:", type(primer_emprendimiento["nombre"]))
 
